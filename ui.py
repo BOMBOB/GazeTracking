041b9714ff3a1cb15bb5a35558349e5edc6786f9
Vachara Suwansophon <requiem202@hotmail.com>
2019-06-19 18:33:03 +0700

    UI completed. Move on to actions.

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 import cv2.cv2 as cv2
 from models import model2
 
@@ -6,29 +7,77 @@ EYE_POSITION_LEFT = 1
 EYE_POSITION_CENTER = 2
 EYE_POSITION_RIGHT = 3
 SELECTED_CYCLE_THRESHOLD = 20
+SELECT_COLOR = {} # pre-generated when application start
 
-# menus = {
-#     'L1M1': './menu/L1M1.png',
-#     'L2M1': './menu/L2M1.png',
-#     'L2M2': './menu/L2M2.png',
-#     'L2M3': './menu/L2M3.png',
-#     'L2MF': './menu/L2MF.png'
-# }
+WINDOW_TITLE = "Eye On Me"
+BACKGROUND_COLOR = 217
+
+ICON_WIDTH = 345
+ICON_HEIGHT = 295
+
+ICON_LEFT_X1 = 40
+ICON_LEFT_Y1 = 161
+ICON_LEFT_X2 = ICON_LEFT_X1 + ICON_WIDTH
+ICON_LEFT_Y2 = ICON_LEFT_Y1 + ICON_HEIGHT
+
+ICON_CENTER_X1 = 626
+ICON_CENTER_Y1 = 161
+ICON_CENTER_X2 = ICON_CENTER_X1 + ICON_WIDTH
+ICON_CENTER_Y2 = ICON_CENTER_Y1 + ICON_HEIGHT
+
+
+ICON_RIGHT_X1 = 1215
+ICON_RIGHT_Y1 = 161
+ICON_RIGHT_X2 = ICON_RIGHT_X1 + ICON_WIDTH
+ICON_RIGHT_Y2 = ICON_RIGHT_Y1 + ICON_HEIGHT
+
+FONT = cv2.FONT_HERSHEY_COMPLEX
+FONT_SCALE = 2
+FONT_THICKNESS = 3
 
 menus = {
     'L0': {
-        'src': './menu/L1M1.png',
+        1: {
+            'src': './menu/L0L.png',
+            'text': 'TOILET'
+        },
+        3: {
+            'src': './menu/L0R.png',
+            'text': 'EMERGENCY'
+        },
         'children': {
-            1: 'L1M1',
+            1: 'L1A',
             # 2: 'L2M2',
-            3: 'L1M3'
+            3: 'L1B'
         }
     },
-    'L1M1': {
-        'src': './menu/L2M1.png'
+    'L1A': {
+        1: {
+            'src': './menu/L1AL.png',
+            'text': 'NO'
+        },
+        2: {
+            'src': './menu/L1AC.png',
+            'text': 'BACK'
+        },
+        3: {
+            'src': './menu/L1AR.png',
+            'text': 'YES'
+        }
     },
-    'L1M3': {
-        'src': './menu/L2M3.png'
+    'L1B': {
+        1: {
+            'src': './menu/L1BL.png',
+            'text': 'NO'
+        },
+        2: {
+            'src': './menu/L1BC.png',
+            'text': 'BACK'
+        },
+        3: {
+            'src': './menu/L1BR.png',
+            'text': 'HURRY'
+        }
     }
 }
 
@@ -37,83 +86,244 @@ def run_model(frame):
     return model2(frame)
 
 
-def focus_left(img):
-    cv2.rectangle(img, (40, 161), (385, 456), (0, 255, 0), 15)
+def add_icon_left(img, icon):
+    if icon is None:
+        return None
+    alpha_s = icon[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        img[ICON_LEFT_Y1:ICON_LEFT_Y2, ICON_LEFT_X1:ICON_LEFT_X2, c] = (alpha_s * icon[:, :, c] +
+                                                                        alpha_l * img[ICON_LEFT_Y1:ICON_LEFT_Y2,
+                                                                                  ICON_LEFT_X1:ICON_LEFT_X2, c])
     return img
 
 
-def focus_center(img):
-    cv2.rectangle(img, (626, 161), (972, 456), (0, 255, 0), 15)
+def put_text_left(img, text):
+    if text is None:
+        return None
+    text_size = cv2.getTextSize(text, FONT, fontScale=FONT_SCALE, thickness=FONT_THICKNESS)[0]
+    text_x = int(ICON_LEFT_X1 + ((ICON_WIDTH - text_size[0]) / 2))
+    text_y = int(ICON_LEFT_Y1 + ICON_HEIGHT + text_size[1] + 20)
+    cv2.putText(img, text, (text_x, text_y),
+                fontFace=FONT,
+                fontScale=FONT_SCALE,
+                color=(147, 58, 31),
+                thickness=FONT_THICKNESS,
+                lineType=cv2.LINE_AA)
+
+
+def focus_left(img, current_percent):
+    # l_base = 29
+    # l_max = 90
+    # l_current = ((l_max - l_base) * current_percent) + l_base
+    #
+    # hsv = np.uint8([[[(120 / 360) * 180, (58 / 100) * 255, (l_current/ 100) * 255]]])
+    # bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    # c = (int(bgr[0][0][0]), int(bgr[0][0][1]), int(bgr[0][0][2]))
+    c = SELECT_COLOR[int(current_percent * 100)]
+    cv2.rectangle(img, (ICON_LEFT_X1, ICON_LEFT_Y1), (ICON_LEFT_X2, ICON_LEFT_Y2), c, cv2.FILLED)
     return img
 
 
-def focus_right(img):
-    cv2.rectangle(img, (1215, 161), (1560, 456), (0, 255, 0), 15)
+def add_icon_center(img, icon):
+    if icon is None:
+        return None
+    alpha_s = icon[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        img[ICON_CENTER_Y1:ICON_CENTER_Y2, ICON_CENTER_X1:ICON_CENTER_X2, c] = (alpha_s * icon[:, :, c] +
+                                                                        alpha_l * img[ICON_CENTER_Y1:ICON_CENTER_Y2,
+                                                                                  ICON_CENTER_X1:ICON_CENTER_X2, c])
+    return img
+
+
+def put_text_center(img, text):
+    if text is None:
+        return None
+    text_size = cv2.getTextSize(text, FONT, fontScale=FONT_SCALE, thickness=FONT_THICKNESS)[0]
+    text_x = int(ICON_CENTER_X1 + ((ICON_WIDTH - text_size[0]) / 2))
+    text_y = int(ICON_LEFT_Y1 + ICON_HEIGHT + text_size[1] + 20)
+    cv2.putText(img, text, (text_x, text_y),
+                fontFace=FONT,
+                fontScale=FONT_SCALE,
+                color=(147, 58, 31),
+                thickness=FONT_THICKNESS,
+                lineType=cv2.LINE_AA)
+    
+    
+def focus_center(img, current_percent):
+    # cv2.rectangle(img, (626, 161), (972, 456), (0, 255, 0), 15)
+
+    c = SELECT_COLOR[int(current_percent * 100)]
+    cv2.rectangle(img, (ICON_CENTER_X1, ICON_CENTER_Y1), (ICON_CENTER_X2, ICON_CENTER_Y2), c, cv2.FILLED)
+
+    return img
+
+
+def add_icon_right(img, icon):
+    if icon is None:
+        return None
+    alpha_s = icon[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        img[ICON_RIGHT_Y1:ICON_RIGHT_Y2, ICON_RIGHT_X1:ICON_RIGHT_X2, c] = (alpha_s * icon[:, :, c] +
+                                                                            alpha_l * img[ICON_RIGHT_Y1:ICON_RIGHT_Y2,
+                                                                                      ICON_RIGHT_X1:ICON_RIGHT_X2, c])
+    return img
+
+
+def put_text_right(img, text):
+    if text is None:
+        return None
+    text_size = cv2.getTextSize(text, FONT, fontScale=FONT_SCALE, thickness=FONT_THICKNESS)[0]
+    text_x = int(ICON_RIGHT_X1 + ((ICON_WIDTH - text_size[0]) / 2))
+    text_y = int(ICON_RIGHT_Y1 + ICON_HEIGHT + text_size[1] + 20)
+    cv2.putText(img, text, (text_x, text_y),
+                fontFace=FONT,
+                fontScale=FONT_SCALE,
+                color=(147, 58, 31),
+                thickness=FONT_THICKNESS,
+                lineType=cv2.LINE_AA)
+
+
+def focus_right(img, current_percent):
+    c = SELECT_COLOR[int(current_percent * 100)]
+    cv2.rectangle(img, (ICON_RIGHT_X1, ICON_RIGHT_Y1), (ICON_RIGHT_X2, ICON_RIGHT_Y2), c, cv2.FILLED)
     return img
 
 
 def get_next_menu(current_menu, eye_position):
     current_menu_item = menus[current_menu]
+    if 'children' not in current_menu_item:
+        return None
     next_menu = current_menu_item['children'][eye_position]
     return next_menu
 
 
-def get_menu_image(menu):
-    return menus[menu]['src']
+# def get_menu_image(menu):
+#     return menus[menu]['src']
+
+def get_icon(menu, eye_position):
+    if eye_position not in menus[menu]:
+        return None
+    return menus[menu][eye_position]['src']
+
+
+def get_text(menu, eye_position):
+    if eye_position not in menus[menu]:
+        return None
+    return menus[menu][eye_position]['text']
+
+
+def get_left_icon(menu):
+    return get_icon(menu, EYE_POSITION_LEFT)
+
+
+def get_left_text(menu):
+    return get_text(menu, EYE_POSITION_LEFT)
+
+
+def get_center_icon(menu):
+    return get_icon(menu, EYE_POSITION_CENTER)
+
+
+def get_center_text(menu):
+    return get_text(menu, EYE_POSITION_CENTER)
+
+
+def get_right_icon(menu):
+    return get_icon(menu, EYE_POSITION_RIGHT)
+
+
+def get_right_text(menu):
+    return get_text(menu, EYE_POSITION_RIGHT)
+
+
+def has_button_on_position(current_menu, eye_position):
+    return current_menu in menus and eye_position in menus[current_menu]
 
 
 def main():
-
     # initial system config
+    # pre-generate colors
+    for i in range(0, 101):
+        l_current = min(100, int((90 - 29) * i / 100) + 29)
+        hsv = np.uint8([[[60, 148, (l_current / 100) * 255]]])
+        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        c = (int(bgr[0][0][0]), int(bgr[0][0][1]), int(bgr[0][0][2]))
+        SELECT_COLOR[i] = c
+
     current_menu = 'L0'
-    # i = 0
-    # countDict = {'cLeft': 0, 'cRight': 0, 'cCenter': 0, 'cBlinking': 0}
     count_dict = {
         EYE_POSITION_LEFT: 0,
         EYE_POSITION_CENTER: 0,
         EYE_POSITION_RIGHT: 0
     }
+    img = np.zeros((900, 1600, 4), dtype=np.uint8)
+    img[:, :, :3] = BACKGROUND_COLOR
+    cv2.imshow(WINDOW_TITLE, img)
 
     webcam = cv2.VideoCapture(0)
 
     try:
         while True:
-            # print(str(menus[current_menu]))
-            img = cv2.imread(get_menu_image(current_menu), cv2.IMREAD_COLOR)
+
+            img = np.zeros((900, 1600, 4), dtype=np.uint8)
+            img[:, :, :3] = BACKGROUND_COLOR
 
             _, frame = webcam.read()
             eye_position = run_model(frame)
 
+            if not has_button_on_position(current_menu, eye_position):
+                continue
+
             if eye_position == EYE_POSITION_LEFT:
                 count_dict[EYE_POSITION_LEFT] += 1
-                focus_left(img)
+                focus_left(img, count_dict[EYE_POSITION_LEFT] / SELECTED_CYCLE_THRESHOLD)
                 print('LEFT')
 
-            # elif eye_position == EYE_POSITION_CENTER:
-            #     count_dict[EYE_POSITION_CENTER] += 1
-            #     focus_center(img)
-            #     print('CENTER')
+            elif eye_position == EYE_POSITION_CENTER:
+                count_dict[EYE_POSITION_CENTER] += 1
+                focus_center(img, count_dict[EYE_POSITION_CENTER] / SELECTED_CYCLE_THRESHOLD)
+                print('CENTER')
 
             elif eye_position == EYE_POSITION_RIGHT:
                 count_dict[EYE_POSITION_RIGHT] += 1
-                focus_right(img)
+                focus_right(img, count_dict[EYE_POSITION_RIGHT] / SELECTED_CYCLE_THRESHOLD)
                 print('RIGHT')
 
             # elif eye_position == 0:
             #     Blink
-                # countDict['cBlinking'] += 1
-                # print('Blink')
+            # countDict['cBlinking'] += 1
+            # print('Blink')
 
             else:
                 print(eye_position)
+
+            left_icon = cv2.imread(get_left_icon(current_menu), cv2.IMREAD_UNCHANGED)
+            add_icon_left(img, left_icon)
+            put_text_left(img, get_left_text(current_menu))
+
+            center_icon = cv2.imread(get_center_icon(current_menu), cv2.IMREAD_UNCHANGED)
+            add_icon_center(img, center_icon)
+            put_text_center(img, get_center_text(current_menu))
+
+            right_icon = cv2.imread(get_right_icon(current_menu), cv2.IMREAD_UNCHANGED)
+            add_icon_right(img, right_icon)
+            put_text_right(img, get_right_text(current_menu))
 
             if count_dict[eye_position] >= SELECTED_CYCLE_THRESHOLD:
                 next_menu = get_next_menu(current_menu, eye_position)
                 if next_menu is not None:
                     current_menu = next_menu
+                else:
+                    break
                 count_dict = count_dict.fromkeys(count_dict, 0)
 
-            cv2.imshow('Eye On Me', img)
+            cv2.imshow(WINDOW_TITLE, img)
             print(count_dict)
             time.sleep(0.3)
 
