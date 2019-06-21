8f144ad9c36affba6d66e26be219ae74af41bd8b
Vachara Suwansophon <requiem202@hotmail.com>
2019-06-21 21:55:43 +0700

    added LINE.

--- messaging.py
+++ messaging.py
@@ -4,13 +4,16 @@ from linebot.exceptions import LineBotApiError
 
 
 class Line:
-    def __init__(self, key, recipient):
+    def __init__(self, key, recipient, dry):
         self.key = key
         self.recipient = recipient
+        self.dry = dry
         self.api = LineBotApi(key)
 
     def send(self, text):
         try:
-            self.api.push_message(self.recipient, TextSendMessage(text=text))
+            if not self.dry:
+                self.api.push_message(self.recipient, TextSendMessage(text=text))
+            print('LINE message sent.', text)
         except LineBotApiError as e:
             print(e)
